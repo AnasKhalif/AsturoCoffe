d3928c402533baa24e034999678c8da9cc021c5e
AnasKhalif <anaskhalif995@gmail.com>
2024-10-08 07:55:28 +0700

    feat : add hero section home

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,13 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + Tailwind</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;400;600;800&display=swap"
+        rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
 
 <body>
     <header class="flex justify-between px-5 lg:px-14 h-[10vh] align-middle items-center bg-white border-b-0 top-0">
         <img width="150" src="{{ asset('images/logo.png') }}" alt="">
-        <nav class="items-center h-full align-middle gap-10 lg:flex">
+        <nav class="items-center h-full align-middle gap-10 lg:flex hidden">
             <div>
                 <a href="" aria-current="page" class="border-b-2 border-black">Home</a>
             </div>
@@ -34,6 +38,30 @@
             </a>
         </div>
     </header>
+
+    <section
+        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url({{ asset('images/asturo_home.jpg') }})"
+        class="flex flex-col lg:flex-row h-[90vh] w-full lg:h-screen bg-cover bg-center items-center lg:px-14 lg:gap-10 align-middle justify-center lg:justify-start">
+        <div class="m-5 lg:m-0">
+            <h1 class="text-white font-bold font-nakilla text-5xl lg:text-6xl lg:w-6/12">Discover the Universe of Coffee
+                in Every Sip!
+            </h1>
+            <p class="font-plus-jakarta text-[14px] text-white text-sm mt-3 lg:w-7/12">Nikmati aroma dan cita rasa kaya
+                yang mencerminkan keindahan galaksi. Dari kopi pahit yang kaya hingga campuran lembut, setiap seruput
+                dirancang untuk membangkitkan indera Anda dan mengangkat semangat.
+                Terletak di Kediri, Asturo Coffee menawarkan suasana yang estetik dan nyaman, menjadikannya tempat
+                yang sempurna untuk bersantai sambil menikmati kopi berkualitas. Bergabunglah bersama kami
+                dan jelajahi keajaiban rasa kopi yang siap mengantarkan Anda pada petualangan antar bintang yang tiada
+                tara!.</p>
+            <div class="flex">
+                <a class=" font-plus-jakarta bg-[#cfc7be] text-white px-5 py-2 rounded-full mt-5 text-sm"
+                    href="/services">
+                    See Services
+                </a>
+            </div>
+        </div>
+    </section>
+
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                nakilla: ['Nakilla', 'sans-serif'],
+                'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
