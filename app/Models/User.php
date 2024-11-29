3f919bce5e222c2d4609e88e757719680f9768a1
AnasKhalif <anaskhalif995@gmail.com>
2024-11-29 19:18:12 +0700

    feat : install laratrust

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laratrust\Traits\HasRolesAndPermissions;
 
 class User extends Authenticatable
 {
+    use HasRolesAndPermissions;
     use HasFactory, Notifiable;
 
     /**
---
+++ config/laratrust_seeder.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => true,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'superadmin' => [
+            'users' => 'c,r,u,d',
+            'payments' => 'c,r,u,d',
+            'seri' => 'c,r,u,d',
+            'menu' => 'c,r,u,d',
+            'profile' => 'r,u',
+        ],
+        'admin' => [
+            'menu' => 'r,u',
+            'profile' => 'r,u',
+        ],
+        'user' => [
+            'profile' => 'r,u',
+        ],
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete',
+    ],
+];
